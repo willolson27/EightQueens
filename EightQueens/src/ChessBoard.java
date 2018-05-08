169a5bfc585a7c282b49d84d8e2f42ebda933e29
Bob <Bob@10.5.4.79>
2018-05-08 12:09:05 -0400

    v

--- EightQueens/src/ChessBoard.java
+++ EightQueens/src/ChessBoard.java
@@ -12,13 +12,15 @@ public class ChessBoard {
 	
 	private static final int ROWS = 8;
 	private static final int COLS = 8;
-	private static final int WIDTH = 500;
-	private static final int HEIGHT = 500;
+	private static final int WIDTH = 1000;
+	private static final int HEIGHT = 1000;
 	private Color colorOne;
 	private Color colorTwo;
 	private JFrame window;
 
 	public ChessBoard() {
+		   colorOne = Color.RED;
+		   colorTwo = Color.BLACK;
 		buildFrame();
 		
 		window.setVisible(true);
@@ -29,16 +31,34 @@ public class ChessBoard {
 	      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	      window.setSize(new Dimension(WIDTH, HEIGHT));
 	      window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS)); 
+	      addPanels();
 	   } 
 	   
 
 	  public void addPanels() {
-		  
+		  int xSize = 50;
+		  int ySize = 50;
+		  ChessSquarePanel sqr = new ChessSquarePanel(colorOne, true);
+		  int index = 0;
+		  for (int i = 0; i < ROWS; i++) {
+			  for (int j = 0; j < COLS; j++) {
+				  if (index % 2 == 0) {
+					 sqr = new ChessSquarePanel(colorOne, true); 
+				  }
+				  else
+					  sqr = new ChessSquarePanel(colorTwo, true);
+				  
+				  window.add(sqr);
+				  sqr.setBounds((i + 1) * xSize , (j + 1) * ySize, xSize, ySize);
+				  index++;
+			  }
+		  } */
 	  }
 	 
 
 
 	   public static void main(String[] args) {
+		
 	      ChessBoard cb = new ChessBoard();
 	      
 	   }
--- EightQueens/src/ChessSquarePanel.java
+++ EightQueens/src/ChessSquarePanel.java
@@ -1,10 +1,33 @@
 import java.awt.Color;
 
-public class ChessSquarePanel {
+import javax.swing.JPanel;
 
+public class ChessSquarePanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Color background;
+	private boolean isEmpty;
+	
 	public ChessSquarePanel (Color back, boolean empty) {
-		
+		setBackground(back);
+		isEmpty = empty;
 	}
+	
+	public void setColor (Color c) {
+		background = c;
+	}
+	
+	public void setState(boolean empty) {
+		isEmpty = empty;
+	}
+	
+	public boolean isEmpty() {
+		return isEmpty();
+	}
+	
 	
 	 
 	
