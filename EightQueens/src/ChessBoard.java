99d280455a25267798778bf8da1f0e16e6d09a3b
Bob <Bob@DESKTOP-GVDICQ4.fios-router.home>
2018-05-13 11:57:16 -0400

    almost done? maybe

--- EightQueens/src/ChessBoard.java
+++ EightQueens/src/ChessBoard.java
@@ -1,73 +1,405 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+/**
+ * 
+ * @author Will Olson (git: willolson27)
+ * Assignment 9: Eight Queens
+ * Due May 13, 2018
+ *
+ */
 public class ChessBoard {
 
 	
-	
+	//Fields
+	private int n;
 	private static final int ROWS = 8;
 	private static final int COLS = 8;
 	private static final int WIDTH = 1000;
 	private static final int HEIGHT = 1000;
-	private Color colorOne;
-	private Color colorTwo;
+	private Color colorOne = Color.WHITE;
+	private Color colorTwo = Color.BLACK;
 	private JFrame window;
-
+	private final String SOLVED = "Eight Queens Solved! Iteration no: ";
+	private final String RUN = "GO!";
+	private int solNum = 0;
+	private final int MAX_ITER = 1000;
+	
+	private ChessSquarePanel[][] chessGrid = new ChessSquarePanel[ROWS][COLS];
+	
+	private int iter = 0;
+	
+	/**
+	 * creates a new default instance of ChessBoard for Eight Queens
+	 */
 	public ChessBoard() {
-		   colorOne = Color.RED;
-		   colorTwo = Color.BLACK;
+	
+		n = 8;
 		buildFrame();
-		
+		JPanel panelOne = buildGridPanels();
+		JPanel panelTwo = bottomPanel();
+		window.add(panelOne);
+		window.add(panelTwo);
 		window.setVisible(true);
 	}
 	
-	private void buildFrame() {
+	/**
+	 * creates a new instance of ChessBoard for n Queens
+	 * @param queens size of grid/ number of queens to be placed
+	 */
+	public ChessBoard(int queens) {
+		
+		n = queens;
+		buildFrame();
+		JPanel panelOne = buildGridPanels();
+		JPanel panelTwo = bottomPanel();
+		window.add(panelOne);
+		window.add(panelTwo);
+		window.setVisible(true);
+	}
+	
+	/**
+	 * builds the outer frame of this ChessBoard
+	 */
+	public void buildFrame() {
 	      window = new JFrame("Eight Queens");
 	      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      window.setBounds(0, 0 ,WIDTH, HEIGHT);
-	      window.setResizable(false);
-	      addPanels();
+	      window.setSize(new Dimension(WIDTH, HEIGHT));
+	      window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS)); 
+	      
 	   } 
 	   
 
-	  public void addPanels() {
-		  int xSize = 50;
-		  int ySize = 50;
-		  ChessSquarePanel sqr = new ChessSquarePanel(colorOne, true);
-		  int index = 0;
-	/*	  for (int i = 0; i < ROWS; i++) {
-			  for (int j = 0; j < COLS; j++) {
-				  if (index % 2 == 0) {
-					 sqr = new ChessSquarePanel(colorOne, true); 
-				  }
-				  else
-					  sqr = new ChessSquarePanel(colorTwo, true);
-				  
-				  window.add(sqr);
-				  sqr.setBounds((i + 1) * xSize , (j + 1) * ySize, xSize, ySize);
-				  index++;
-			  }
-		  } */
-		  sqr.setBounds(0, 0, 50, 50);
-		  
-		  ChessSquarePanel sqr2 = new ChessSquarePanel(colorTwo, true);
-		  sqr2.setBounds(0, 50, 50, 50);
-		  window.add(new ChessSquarePanel(Color.white, true));
-		  window.add(sqr);
-		  window.add(sqr2);
-	  }
+	/**
+	 * builds the JPanel containing a n*n grid of ChessSquarePanels
+	 * @return
+	 */
+	public JPanel buildGridPanels() {
+	      JPanel grid = new JPanel();
+	      grid.setLayout(new GridLayout(ROWS,COLS));
+	      Color bg;
+	      for (int r = 0; r < ROWS; r++) {
+	         for (int c = 0; c < COLS; c++) {
+	            bg = setPanelColor(r,c);
+	            ChessSquarePanel s = new ChessSquarePanel(bg, false);
+	            chessGrid[r][c] = s;  
+	            grid.add(s);	           
+	         }
+	      }
+	      return grid;
+	   }
+	
+	/**
+	 * builds the bottom panel of this ChessBoard - contains a activation button
+	 * @return Bottom JPanel
+	 */
+	public JPanel bottomPanel() {
+		JPanel bottom = new JPanel();
+		JButton go = new JButton(RUN);
+		go.addActionListener(e -> action());
+		bottom.add(go);
+		return bottom;
+	}
+	
+	/**
+	 * Runs the n Queens program, called when the button is pressed
+	 */
+	public void action() {
+		
+		this.clear();
+		ArrayList<Integer> unusedCols = new ArrayList<Integer>();
+		for (int i = 0; i < 8; i++) {
+			unusedCols.add(i);
+		} 
+		System.out.println(this.catchOverflow(unusedCols));
+		
+	}
+	
+	/**
+	 * sets the panel color of a given chess square to color one or color two based on location
+	 * @param r y coordinate of location
+	 * @param c c coordinate of location
+	 * @return Color to be set to a given chess panel
+	 */
+	public Color setPanelColor(int r, int c) {
+			int indexR = r % 2;
+			int indexC = c % 2;
+			if (indexR == indexC)
+				return colorOne;
+			else
+				return colorTwo;
+		}
 	 
-
+	 /**
+	  * checks if a placement at a given coordinate is valid/legal
+	  * @param row - y coordinate of placement
+	  * @param col - x coordinate of placement
+	  * @return boolean : true if legal, false if not
+	  */
+	   public boolean isValid(int row, int col) {
+		  
+		  //check vertically 
+		  for (int r = 0; r < ROWS; r++)
+			  if (chessGrid[r][col].hasQueen())
+				  return false;
+		  
+		  //check horizontally
+		  for (int c = 0; c < COLS; c++) 
+			  if (chessGrid[row][c].hasQueen())
+				  return false;
+			  
+		  //Check diagonally
+		  for (int r = row, c = col; r < 8 && c < 8; r++, c++)
+			  if (chessGrid[r][c].hasQueen()) 
+				  return false;
+		  for (int r = row, c = col; r < 8 && c >= 0; r++, c--)
+			  if (chessGrid[r][c].hasQueen()) 
+				  return false;
+		  for (int r = row, c = col; r >= 0 && c < 8; r--, c++)
+			  if (chessGrid[r][c].hasQueen()) 
+				  return false;
+		  for (int r = row, c = col; r >= 0 && c >= 0; r--, c--)
+			  if (chessGrid[r][c].hasQueen()) 
+				  return false;
+			  
+			  
+		   return true;
+	   }
+	   
+/*	   public int getDiagLength(int row, int col) {
+		   
+		   int length = 0;
+		   
+		   if (row > col)
+			   return 8 - (row - col);
+		   else if (col > row)
+			   return 8 - (row - col);
+		   return 8;
+		   
+		   
+		   
+	   }
+		
+ 	   private boolean solve(ArrayList<int[]> positions) {
+		   
+		   if (positions.size() == 8)
+			   return true;
+		   
+		   for (int r = 0; r < ROWS; r++) {
+		         for (int c = 0; c < COLS; c++) {
+		          if (isValid(r,c)) {
+		        	  chessGrid[r][c].setState(true);
+		        	  int[] pos = {r,c};
+		        	  positions.add(pos);
+		        	  if (solve(positions) == true)
+		        		  return true;
+		        	  else
+		        		  positions.remove(pos);
+		          }
+	  
+		         }
+		      }
+		   
+		   return false;
+		   
+	   } 
+	   
+	 private boolean solve(ArrayList<Integer> cols, int row) {
+		  
+		  if (row == -1)
+			  return true;
+		  if (cols.size() == 0)
+			  return true;
+		  
+		  int a = (int)(Math.random() * cols.size() - 1);
+		 
+		  int c = cols.get(a);
+		  int r = 0;
+		  if (isValid(row, c)) {
+			  cols.remove(cols.get(a));
+			  chessGrid[row][c].setState(true);
+			  if (solve(cols, row - 1) == true) {
+				  chessGrid[row][c].setState(true);
+				  return true;
+			  }
+			  else {
+		//		if (iter >= MAX_ITER)
+		//		   return false;
+				  cols.add(c);
+			  	chessGrid[row][c].setState(false);
+			  	iter++;
+			  	return solve (cols, row);
+			  
+			  }
+		  }
+			  
+		   row++;
+		   return false;
+		   
+	   } 
+	 
+	 private boolean solve(ArrayList<Integer> cols, int row) {
+		  
+		  if (row == -1)
+			  return true;
+		  if (cols.size() == 0)
+			  return true;
+		  ArrayList<Integer> temp = randOrder(cols);
+		  cols = (ArrayList<Integer>) temp.clone();
+		  Integer c = 0;
+		  
+		  Iterator<Integer> iter = temp.iterator();
+		  while(iter.hasNext()) {
+			  c = iter.next();
+			  if (isValid(row, c)) {
+			      iter.remove();
+				  chessGrid[row][c].setState(true);
+				  if (solve(temp, row - 1) == true) {
+					  
+					  return true;
+				  }
+				  else {
+				  chessGrid[row][c].setState(false);
+				  temp.add(c);
+				  }
+			  }
+		  }
+			  
+		   row++;
+		   return false;
+		   
+	   } 
+	   
+	   private boolean solve(ArrayList<Integer> cols, int row) {
+			  
+			  if (row == -1)
+				  return true;
+			  if (cols.size() == 0)
+				  return true;
+			  
+			  int a = (int)(Math.random() * cols.size() - 1);
+			 
+			  int c = cols.get(a);
+			  int r = 0;
+			  if (isValid(row, c)) {
+				  cols.remove(cols.get(a));
+				  chessGrid[row][c].setState(true);
+				  if (solve(cols, row - 1) == true) {
+					  chessGrid[row][c].setState(true);
+					  return true;
+				  }
+				  else {
+					  cols.add(c);
+				  	chessGrid[row][c].setState(false);
+			//	  	iter++;
+			//	  	return solve (cols, row);
+				  
+				  }
+			  }
+				  
+			   row++;
+			   return false;
+			   
+		   }  */
+	
+	   private boolean solve(ArrayList<Integer> cols, int row) {
+			  
+			  if (row == -1)
+				  return true;
+			  if (cols.size() == 0)
+				  return true;
+			  
+			  int a = (int)(Math.random() * cols.size() - 1);
+			 
+			  int c = cols.get(a);
+			  
+			  if (isValid(row, c)) {
+				  cols.remove(cols.get(a));
+				  chessGrid[row][c].setState(true);
+				  if (solve(cols, row - 1) == true) {
+					  chessGrid[row][c].setState(true);
+					  return true;
+				  }
+				  else {
+					cols.add(c);
+				  	chessGrid[row][c].setState(false);
+				  	iter++;
+				  	return solve (cols, row);
+				  }
+			  }
+				  
+			   row++;
+			   return false;
+			   
+		   } 
+	   
+		public ArrayList<Integer> randOrder(ArrayList<Integer> old) {
+			
+			ArrayList<Integer> cols = new ArrayList<Integer>();
+			
+			while (!old.isEmpty()) {
+				int a = (int)(Math.random() * old.size() - 1);
+				cols.add(old.get(a));
+				old.remove(a);
+			}
+			
+			return cols;
+		}
+	 
+	 	private String catchOverflow(ArrayList<Integer> cols) {
+	 		
+	 		try{
+	            solve(cols, n - 1);
+	        }
+	        catch(StackOverflowError e){
+	         /*   this.clear();
+	            return catchOverflow(cols); */
+	        	return "failed";
+	        }
+	 		solNum += 1;
+	 		return SOLVED + solNum;
+	 		
+	 	}
+	 	
+	 	
+	   /**
+	    * removes all queens from the board
+	    */
+	   public void clear () {
+	 		for (int r = 0; r < ROWS; r++) {
+	 			for (int c = 0; c < COLS; c++) {
+	 				chessGrid[r][c].setState(false);
+	 			}
+	 		}
+	 	}
+	   
+	   
+	   public boolean solve() {
+		   
+		   
+		   return false;
+	   }
 
 	   public static void main(String[] args) {
 		
 	      ChessBoard cb = new ChessBoard();
-	      
+/*	//      ArrayList<int[]> pos = new ArrayList<int[]>();
+	    
+	      ArrayList<Integer> unusedCols = new ArrayList<Integer>();
+	//      boolean[] cols = new boolean[8];
+	      for (int i = 0; i < 8; i++) {
+	    //	  cols[i] = false;
+	    	  unusedCols.add(i);
+	      } 
+	      System.out.println(cb.catchOverflow(unusedCols));
+
+	     */
 	   }
 
 	
--- EightQueens/src/ChessSquarePanel.java
+++ EightQueens/src/ChessSquarePanel.java
@@ -1,31 +1,73 @@
 import java.awt.Color;
-
+import java.awt.Font;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
+/**
+ * 
+ * @author Will Olson (git: willolson27)
+ * Assignment 9: Eight Queens
+ * Due May 13, 2018
+ *
+ */
 public class ChessSquarePanel extends JPanel{
 
-	/**
-	 * 
-	 */
+	//FIELDS
 	private static final long serialVersionUID = 1L;
+	private static final int FONTSIZE = 50;
 	private Color background;
-	private boolean isEmpty;
+	private boolean hasQueen;
 	
-	public ChessSquarePanel (Color back, boolean empty) {
-		setBackground(back);
-		isEmpty = empty;
+	/**
+	 * constructor for new Chess Panel based on a given color and state
+	 * @param back color to be set to background color
+	 * @param queen whether or not this panel has a queen in it
+	 */
+	public ChessSquarePanel (Color back, boolean queen) {
+		background = back;
+		hasQueen = queen;
 	}
 	
+	/**
+	 * updates the paint component of the chess square panel, draws a Q if hasQueen is true
+	 */
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+
+	    g.setFont(new Font("TimesRoman", Font.PLAIN, FONTSIZE));
+	    this.setBackground(background);
+	    g.setColor(Color.RED);
+
+	    int x = (this.getWidth() / 2) - FONTSIZE/4;
+	    int y = (this.getHeight() / 2) + FONTSIZE/4;
+	    if (hasQueen)
+	    	g.drawString("Q", x, y);
+	  }
+	
+	/**
+	 * sets the background color of this panel to a new color
+	 * @param c new color for background
+	 */
 	public void setColor (Color c) {
 		background = c;
+		repaint();
 	}
 	
-	public void setState(boolean empty) {
-		isEmpty = empty;
+	/**
+	 * sets the state (whether it has a queen) of this panel to a given state
+	 * @param queen true to change to queen, false to change to empty
+	 */
+	public void setState(boolean queen) {
+		hasQueen = queen;
+		repaint();
 	}
 	
-	public boolean isEmpty() {
-		return isEmpty();
+	/**
+	 * checks if this panel has a queen in it
+	 * @return true if panel has queen, false if empty
+	 */
+	public boolean hasQueen() {
+		return hasQueen;
 	}
 	
 	
